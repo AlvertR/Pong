9f78dced0282d3315f0b694af3efb24e2516a9d4
AlvertR <alvis99laj@gmail.com>
2026-09-14 12:45:40 -0600

    -> fix collisions, use vector2, implement deltaTime, add image for icon

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Raylib_cs;
+using System.Numerics;
 
 enum GameStatus
 {
@@ -7,7 +8,8 @@ enum GameStatus
     Continue,
     End
 }
-public class Program {
+public class Program
+{
 
     public Program() { }
     public static void Main(string[] args)
@@ -15,22 +17,27 @@ public class Program {
         const int heightWindow = 450, widthWindow = 800;
 
         Raylib.InitWindow(widthWindow, heightWindow, "Pong");
+        //string basePath = AppDomain.CurrentDomain.BaseDirectory;
+        //string fulPath = Path.Combine(basePath, "Resource", "pong-image.png");
+        //Image icon = Raylib.LoadImage(fulPath);
+        //Raylib.SetWindowIcon(icon);
+        //Raylib.UnloadImage(icon);
         Raylib.SetTargetFPS(60);
 
-        const int racketHeight = 100, racketWidth = 30, leftRacketX = 40, rightRacketX = widthWindow - racketWidth - leftRacketX, ballRadius = 30;
-        int leftRacketY = 10;
-        int rightRacketY = 10;
-        int ballX = 400;
-        int ballY = 225;
-        bool ballIsDown = true;
-        bool ballIsRight = true;
-        int ballSpeed = 2;
+        const int racketHeight = 100, racketWidth = 30, ballRadius = 30;
+        Vector2 leftRacket = new Vector2(40, 10);
+        Vector2 rightRacket = new Vector2(widthWindow - racketWidth - leftRacket.X, 10);
+        Vector2 ballPosition = new Vector2(400, 225);
+        Vector2 ballVelocity = new Vector2(120, 120);
+        float deltaTime = 0;
         int leftPlayerScore = 0;
         int rightPlayerScore = 0;
+        float racketSpeed = 600;
         GameStatus gameState = GameStatus.Start;
 
         while (!Raylib.WindowShouldClose())
         {
+            deltaTime = Raylib.GetFrameTime();
             HandleInput();
             Update();
             Draw();
@@ -38,27 +45,28 @@ public class Program {
 
         void HandleInput()
         {
-            switch(gameState){
+            switch (gameState)
+            {
                 case GameStatus.Start:
                     if (Raylib.IsKeyPressed(KeyboardKey.I))
                         gameState = GameStatus.Playing;
                     break;
                 case GameStatus.Playing:
-                    if (Raylib.IsKeyDown(KeyboardKey.Up) && rightRacketY > 0)
-                        rightRacketY -= 10;
-                    if (Raylib.IsKeyDown(KeyboardKey.Down) && rightRacketY < heightWindow - racketHeight - 1)
-                        rightRacketY += 10;
+                    if (Raylib.IsKeyDown(KeyboardKey.Up) && rightRacket.Y > 0)
+                        rightRacket.Y -= racketSpeed * deltaTime;
+                    if (Raylib.IsKeyDown(KeyboardKey.Down) && rightRacket.Y < heightWindow - racketHeight - 1)
+                        rightRacket.Y += racketSpeed * deltaTime;
 
-                    if (Raylib.IsKeyDown(KeyboardKey.W) && leftRacketY > 0)
-                        leftRacketY -= 10;
-                    if (Raylib.IsKeyDown(KeyboardKey.S) && leftRacketY < heightWindow - racketHeight - 1)
-                        leftRacketY += 10;
+                    if (Raylib.IsKeyDown(KeyboardKey.W) && leftRacket.Y > 0)
+                        leftRacket.Y -= racketSpeed * deltaTime;
+                    if (Raylib.IsKeyDown(KeyboardKey.S) && leftRacket.Y < heightWindow - racketHeight - 1)
+                        leftRacket.Y += racketSpeed * deltaTime;
                     break;
                 case GameStatus.Continue:
                     if (Raylib.IsKeyPressed(KeyboardKey.C))
                     {
-                        ballX = 400;
-                        ballY = 225;
+                        ballPosition.X = 400;
+                        ballPosition.Y = 225;
                         gameState = GameStatus.Playing;
                     }
                     if (Raylib.IsKeyPressed(KeyboardKey.E))
@@ -72,36 +80,38 @@ public class Program {
 
         void MoveBall()
         {
-            if (ballIsDown)
-                ballY += ballSpeed;
-            else ballY -= ballSpeed;
+            //ballY += ballVelocityY * deltaTime;
+            //ballX += ballVelocityX * deltaTime;
+            ballPosition += ballVelocity * deltaTime;
 
-            if (ballIsRight)
-                ballX += ballSpeed;
-            else ballX -= ballSpeed;
-
-            if (ballY <= 1 + ballRadius || ballY >= heightWindow - ballRadius - 1)
-                ballIsDown = !ballIsDown;
+            if (ballPosition.Y <= 1 + ballRadius || ballPosition.Y >= heightWindow - ballRadius - 1)
+            {
+                ballVelocity.Y = ballVelocity.Y * -1;
+            }
 
             //if (ballX <= 1 + ballRadius || ballX >= widthWindow - ballRadius - 1)
             //    ballIsRight = !ballIsRight;
 
-            if (ballX <= leftRacketX + racketWidth + 1)
+            if (ballPosition.X - ballRadius <= leftRacket.X + racketWidth + 1)
             {
-                if (ballY >= leftRacketY + 1 && ballY <= leftRacketY + racketHeight)
-                    ballIsRight = !ballIsRight;
-                else if (ballX <= 1)
+                if (ballPosition.Y + ballRadius >= leftRacket.Y + 1 && ballPosition.Y - ballRadius <= leftRacket.Y + racketHeight)
+                {
+                    ballVelocity.X = ballVelocity.X * -1;
+                }
+                else if (ballPosition.X - ballRadius <= 1)
                 {
                     rightPlayerScore++;
                     gameState = GameStatus.Continue;
                 }
             }
 
-            if (ballX >= rightRacketX + 1)
+            if (ballPosition.X + ballRadius >= rightRacket.X + 1)
             {
-                if (ballY >= rightRacketY + 1 && ballY <= rightRacketY + racketHeight)
-                    ballIsRight = !ballIsRight;
-                else if (ballX >= widthWindow - 1)
+                if (ballPosition.Y + ballRadius >= rightRacket.Y + 1 && ballPosition.Y - ballRadius <= rightRacket.Y + racketHeight)
+                {
+                    ballVelocity.X = ballVelocity.X * -1;
+                }
+                else if (ballPosition.X + ballRadius >= widthWindow - 1)
                 {
                     leftPlayerScore++;
                     gameState = GameStatus.Continue;
@@ -128,31 +138,31 @@ public class Program {
             switch (gameState)
             {
                 case GameStatus.Start:
-                        Raylib.DrawText("Presiona I para inciar", widthWindow / 4, heightWindow / 2, 42, Color.White);
+                    Raylib.DrawText("Presiona I para inciar", 10, heightWindow / 2, 42, Color.White);
                     break;
                 case GameStatus.Playing:
-                        Raylib.DrawCircle(ballX, ballY, ballRadius, Color.White);
-                        Raylib.DrawRectangle(leftRacketX, leftRacketY, racketWidth, racketHeight, Color.White);
-                        Raylib.DrawRectangle(rightRacketX, rightRacketY, racketWidth, racketHeight, Color.White);
-                        Raylib.DrawText("Jugador 1: " + leftPlayerScore.ToString(), leftRacketX, 5, 24, Color.White);
-                        Raylib.DrawText("Jugador 2: " + rightPlayerScore.ToString(), rightRacketX, 5, 24, Color.White);
+                    Raylib.DrawCircleV(ballPosition, ballRadius, Color.White);
+                    Raylib.DrawRectangleV(leftRacket, new Vector2 (racketWidth, racketHeight), Color.White);
+                    Raylib.DrawRectangleV(rightRacket, new Vector2(racketWidth, racketHeight), Color.White);
+                    Raylib.DrawText("Jugador 1: " + leftPlayerScore.ToString(), 40, 5, 24, Color.White);
+                    Raylib.DrawText("Jugador 2: " + rightPlayerScore.ToString(), 650, 5, 24, Color.White);
                     break;
                 case GameStatus.Continue:
-                    Raylib.DrawText("Presiona C para continuar", widthWindow / 5, heightWindow / 2, 42, Color.White);
-                    Raylib.DrawText("Presiona E para Terminar", widthWindow / 4, (heightWindow / 2) + 60, 42, Color.White);
+                    Raylib.DrawText("Presiona C para continuar", 10, heightWindow / 2, 42, Color.White);
+                    Raylib.DrawText("Presiona E para Terminar", 10, (heightWindow / 2) + 60, 42, Color.White);
                     break;
                 case GameStatus.End:
                     if (rightPlayerScore == leftPlayerScore)
-                        Raylib.DrawText("Empate, " + rightPlayerScore.ToString() + " puntos", 0, heightWindow / 2, 36, Color.White);
+                        Raylib.DrawText("Empate, " + rightPlayerScore.ToString() + " puntos", 10, heightWindow / 2, 36, Color.White);
                     else if (rightPlayerScore > leftPlayerScore)
-                        Raylib.DrawText("Gana el jugador 2 con " + rightPlayerScore.ToString() + " puntos", 0, heightWindow / 2, 36, Color.White);
+                        Raylib.DrawText("Gana el jugador 2 con " + rightPlayerScore.ToString() + " puntos", 10, heightWindow / 2, 36, Color.White);
                     else
-                        Raylib.DrawText("Gana el jugador 1 con " + leftPlayerScore.ToString() + " puntos", 0, heightWindow / 2, 36, Color.White);
+                        Raylib.DrawText("Gana el jugador 1 con " + leftPlayerScore.ToString() + " puntos", 10, heightWindow / 2, 36, Color.White);
                     break;
                 default:
                     break;
             }
-                    Raylib.EndDrawing();
+            Raylib.EndDrawing();
         }
         Raylib.CloseWindow();
     }
